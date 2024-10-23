fdd82acaf4a80f09ca75c20e8dca0571bce0e3bc
Samir <samirmh15@gmail.com>
2024-10-23 18:20:37 +0200

    Se realizó endponits

--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -12,4 +12,41 @@ let allCities =  async (req,res,next) => {
 
 }
 
-export {allCities}
+
+let idCities =  async (req,res) => {
+    try {
+
+        console.log(req.params);
+
+        let idQuery = req.params.id
+        let all = await City.findById(idQuery)
+        return res.status(200).json({
+            response:all
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+
+let cityFilter = async (req,res,next) => {
+    try {
+        
+        let{name}= req.query
+        let query ={} 
+
+        if (name) {
+            query.name = {$regex:'^'+ name, $options: 'i'} 
+        }
+
+        let filter = await City.find(query)
+        return res.status(200).json({
+            response:filter
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+export {allCities,cityFilter,idCities}
