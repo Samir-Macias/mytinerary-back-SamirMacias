d6b9faf6aa55c134cde32fa54661bcc5e170522b
Samir <samirmh15@gmail.com>
2024-11-01 22:59:35 +0100

    Se crearon rutas de delete para modelo city

--- controllers/cities/deleate.js
+++ controllers/cities/deleate.js
@@ -1,1 +1,35 @@
 import City from "../../models/City.js";
+
+
+let deleteCity = async (req, res, next) => {
+    try {
+
+        let deletecities = await City.deleteOne({
+            name: req.body.name
+        })
+        return res.status(200).json({
+            response: deletecities
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+
+let deleteAllCities = async (req, res, next) => {
+    try {
+
+        let deletecities = await City.deleteMany({
+            name: req.body.name
+        })
+        return res.status(200).json({
+            response: deletecities
+        })
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+export { deleteCity, deleteAllCities } 
--- router/cities.js
+++ router/cities.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { allCities, cityFilter, idCities } from "../controllers/cities/read.js";
 import { create } from "../controllers/cities/create.js";
 import { updateCities, updateManyCities } from "../controllers/cities/update.js";
+import { deleteCity, deleteAllCities } from "../controllers/cities/deleate.js";
 
 
 const router = Router()
@@ -10,7 +11,9 @@ router.get('/all', allCities)
 router.get('/id/:id', idCities)
 router.get('/filter', cityFilter)
 router.post('/create', create)
-router.put('update',updateCities)
-router.put('updateall',updateManyCities)
+router.put('update', updateCities)
+router.put('updateall', updateManyCities)
+router.delete('delete', deleteCity)
+router.delete('deleteall', deleteAllCities)
 
 export default router
